b344302e563c26e7dbbd588c3dd8d45342de2cf3
Raymond Zhong <ray@kyri.com>
2012-01-27 09:57:27 -0500

    Updated event handling, should work with public/shared dashboards now

--- leftronic_hack.js
+++ leftronic_hack.js
@@ -2,13 +2,18 @@
      Raymond Zhong (hello@raymondzhong.com)
      MIT License */
 chrome.extension.sendRequest({greeting: "get_settings"}, function(response) {
+				 debugger;
 				 console.log(response);
 				 if (response.farewell === undefined) return;
 
 				 var inj = document.createElement('script');
-				 inj.innerText = "LeftronicConfig.gridX = " + response.farewell.gridsizex + ";" +
-				     "LeftronicConfig.gridY = " + response.farewell.gridsizey + ";" +
-				     "dashboardManager.resizeGrid();";
+				 inj.innerText = "function leftronic_hack_size() {" +
+				     "var gx = " + response.farewell.gridsizex + ";" +
+				     "var gy = " + response.farewell.gridsizey + ";" +
+				     "if (gx != LeftronicConfig.gridX || gy != LeftronicConfig.gridY) {" +
+				     "LeftronicConfig.gridX = gx; LeftronicConfig.gridY = gy; dashboardManager.resizeGrid(); }" +
+				     "};" +
+				     "$('#grid').resize(leftronic_hack_size); leftronic_hack_size()";
 				 document.body.parentElement.insertBefore(inj, document.body);
 			     });
 
